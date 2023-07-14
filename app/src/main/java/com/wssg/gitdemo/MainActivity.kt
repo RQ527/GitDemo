07c1a486239ad6408cc5ff057cd5e4f7a3bea431
lytMoon <2936437824@qq.com>
2023-07-14 15:21:54 +0800

    :sparkles: 455555

--- app/src/main/java/com/wssg/gitdemo/MainActivity.kt
+++ app/src/main/java/com/wssg/gitdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.wssg.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TAG", "onCreate: ")
+        ini()
+    }
+
+    private fun ini() {
+        TODO("Not yet implemented")
     }
 }
