cb9a903a34bc407ac25231bb2547cd128be5ce61
RQ527 <1799796122@qq.com>
2023-07-14 15:34:27 +0800

    :sparkles: 打了个日志2

--- app/src/main/java/com/wssg/gitdemo/MainActivity.kt
+++ app/src/main/java/com/wssg/gitdemo/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("TAG", "onCreate: ")
+        Log.d("TAG", "onCreate: ")
     }
 }
