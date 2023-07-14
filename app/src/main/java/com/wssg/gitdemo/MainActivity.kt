b4c23bead72af062c7d686a128a28034f9470479
RQ527 <1799796122@qq.com>
2023-07-14 15:01:18 +0800

    init commit

---
+++ app/src/main/java/com/wssg/gitdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wssg.gitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
